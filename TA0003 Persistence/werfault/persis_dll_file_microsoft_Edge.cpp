28c65aaa5c91fee87c350640c16c3ff7a8f29f86
BlueTrust <92355496+Sohrabian@users.noreply.github.com>
2025-06-11 14:51:55 +0330

    Create persis_dll_file_microsoft_Edge.cpp

---
+++ TA0003 Persistence/werfault/persis_dll_file_microsoft_Edge.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+#include "pch.h"
+#pragma comment (lib, "user32.lib")
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  nReason, LPVOID lpReserved) {
+    switch (nReason) {
+    case DLL_PROCESS_ATTACH:
+        MessageBoxA(NULL, "Meow-meow!!", "=^..^=", MB_OK);
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    }
+    return TRUE;
+}
